34dabb3d1b0964c71088ce428fc9364247e60dc2
urk17me114 <glenpaulcs007@gmail.com>
2025-12-27 21:19:25 +0100

    create automated tests for format currency

---
+++ project/javascript-amazon-project/javascript-amazon-project/tests/moneyTests.js
@@ -0,0 +1,31 @@
+import { formatCurrency } from "../scripts/utils/money.js";    
+
+
+console.log("Testsuite: Format currency");
+
+        /* Test case 1 */
+        if(formatCurrency(2095)==="20.95"){
+            console.log("Test case 1 passed");}
+        else{
+            console.log("Test case 1 failed");
+        }
+
+        /* Test case 2 */
+        if (formatCurrency(0)==="0.00"){
+            console.log("Test case 2 passed");
+        }
+        else{
+            console.log("Test case 3 failed");
+        }
+
+        /* Test case 3 */
+        if (formatCurrency(2000.5)==="20.01"){
+            console.log("Test case 3 passed");
+        }
+        else{
+            console.log("Test case 3 failed");
+            console.log("output is " + formatCurrency(2000.5));
+        }
+
+
+        
