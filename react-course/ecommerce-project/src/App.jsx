d72c2161dc5b776a13ea981d3fc664f80359c8ce
urk17me114 <glenpaulcs007@gmail.com>
2026-03-10 06:58:55 +0100

    Create multiple pages using react-router

--- react-course/ecommerce-project/src/App.jsx
+++ react-course/ecommerce-project/src/App.jsx
@@ -1,13 +1,21 @@
 //import { useState } from 'react'
 import './App.css'
 import {HomePage} from './pages/HomePage.jsx'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-      <HomePage />
+      <Routes> {/* Routes componenet tells React all the pages that are in the website */}
+        
+        {/* To add a page to the website we use route. Route is basically a page */}
+        {/* Here index is same as giving path = "/" */}
+        <Route index element = {<HomePage />}></Route> {/* Works like the url page in django */}
+        <Route path = "checkout" element = {<div>Test checkout page </div>}></Route> {/* Works like the url page in django */}
+     
+     </Routes> 
     </>
   )
 }
