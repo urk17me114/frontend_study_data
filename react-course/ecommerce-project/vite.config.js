a9800d6579764c2d94dfc106c58598118fde86f3
urk17me114 <glenpaulcs007@gmail.com>
2026-03-15 14:22:18 +0100

    Add server proxy config for /api and /images

--- react-course/ecommerce-project/vite.config.js
+++ react-course/ecommerce-project/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { //This is called a server proxy relationship
+    proxy:{
+      '/api':{
+        target: 'http://localhost:3000' /* This means that if the url path starts with /api it the request will automatically 
+                                        go into localhost 3000 */
+      },
+      '/images':{
+        target : 'http://localhost:3000' /* Also in index .html update <base href="/"/> */
+      }
+      
+    }
+  }
 })
